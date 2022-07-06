b8f4d7ab33608e9ed6987ced746587962fd80c52
nikitas <n.kiselev2002@gmail.com>
2022-07-06 21:34:42 +0300

    Done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ExcelBot',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='nkise',
+    author_email='',
+    description=''
+)
